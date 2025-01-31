d1014095ea2685b164bf8a548baeecf85653280a
chakra13 <chatrynguemen@gmail.com>
2025-01-31 18:23:31 +0100

    update phonebook

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-app.use(cors())
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req) => {
@@ -57,20 +71,26 @@ app.get('/info', (request, response) => {
   )
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+      .then(person => {
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
+        }
     })
+    .catch(error => next(error))
   })
   
-app.delete('/api/persons/:id', (request, response) => {
-const id = Number(request.params.id)
-persons = persons.filter(person => person.id !== id)
-
-response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-  
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
@@ -85,19 +105,32 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   }
 
-  // if (persons.find(p => p.name === person.name)) {
-  //   return response.status(400).json({ 
-  //     error: 'name must be unique' 
-  //   })
-  // } else {
-  //     persons = persons.concat(person)
-
-  //     response.json(person)
-  // }
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+// this has to be the last loaded middleware.
+// handler of requests with result to errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
